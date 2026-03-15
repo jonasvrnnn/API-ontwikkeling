1b25e111d41a4aac1ae353561b9e98243cf35430
jonasvrnnn <jonas.verbruggen@student.ap.be>
2026-03-15 20:06:06 +0100

    intro demo herhaling

--- herhaling/herhaling/Models/Student.cs
+++ herhaling/herhaling/Models/Student.cs
@@ -2,5 +2,11 @@
 
 public class Student
 {
-    
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
 }
